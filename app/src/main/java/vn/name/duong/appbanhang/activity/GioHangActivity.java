2674ceb9b645ccb00ec91b72aebdc2ab2ab83853
DESKTOP-FSJ3QBC\LAPTOP DELL <nguyentuanduong1012000@gmail.com>
2022-06-11 13:40:04 +0700

    Xong vd 30

--- app/src/main/java/vn/name/duong/appbanhang/activity/GioHangActivity.java
+++ app/src/main/java/vn/name/duong/appbanhang/activity/GioHangActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,7 @@ public class GioHangActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Button btnmuahang;
     GioHangAdapter adapter;
+    long tongtiensp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +42,7 @@ public class GioHangActivity extends AppCompatActivity {
     }
 
     private void tinhTongTien() {
-        long tongtiensp = 0;
+        tongtiensp = 0;
         for (int i = 0; i<Utils.manggiohang.size(); i++){
             tongtiensp = tongtiensp+ (Utils.manggiohang.get(i).getGiasp()*Utils.manggiohang.get(i).getSoluong());
         }
@@ -69,6 +71,17 @@ public class GioHangActivity extends AppCompatActivity {
             adapter = new GioHangAdapter(getApplicationContext(), Utils.manggiohang);
             recyclerView.setAdapter(adapter);
         }
+
+      // Chuyển từ giỏ hàng sang thanh toán
+        btnmuahang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ThanhToanActivity.class);
+                // Truyền tổng tiền qua bên Thanh toán
+                intent.putExtra("tongtien", tongtiensp);
+                startActivity(intent);
+            }
+        });
 
     }
 
